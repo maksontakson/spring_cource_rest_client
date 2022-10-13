906152a0867f147b5cb58d10822dd2cfd8ca21a5
maksi <maksontuskon@gmail.com>
2022-10-13 15:12:29 +0300

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.example.configuration.MyConfig;
+import org.example.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication", Communication.class);
+
+        communication.deleteEmployee(9);
+    }
+}
